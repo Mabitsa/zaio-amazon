4432b50662da7e8540195b9c5c39fa11fe2564e4
Mabitsa Pennylope <mabitsapennylope@gmail.com>
2024-08-26 21:54:15 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { Switch } from "react-router-dom";
+import Home from "./components/Home";
+import Products from "./components/Products";
+import Header from "./components/layout/Header";
+import ProductDetails from './components/ProductDetails';
 
-function App() {
+const App = ()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <main><Route path="/home">
+        <Home />
+      </Route>
+      <Route path="/products">
+        <Products />
+      </Route>
+      <Route path="/products/:id">
+        <ProductDetails />
+      </Route>
+      </main>
     </div>
   );
 }
 
 export default App;
+
+
